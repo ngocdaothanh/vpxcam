c555996b5cf814890d85b905a82885e829efde63
Ngoc Dao <ngocdaothanh@gmail.com>
2013-12-03 18:00:41 +0900

    Fix comment about force_key_frame

--- server/src/encode.h
+++ server/src/encode.h
@@ -1,8 +1,7 @@
 #ifndef _ENCODE_H_
 #define _ENCODE_H_
 
-/** force_key_frame = 10 means force a key frame every 10 frames; 0 means default */
-bool vpx_init(int width, int height);
+boolean vpx_init(int width, int height);
 
 /** encoded will be filled with encoded data; it must be big enough */
 void vpx_encode(const char* yv12_frame, char* encoded, int* size, bool force_key_frame);
